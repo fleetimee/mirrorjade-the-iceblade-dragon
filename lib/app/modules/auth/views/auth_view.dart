e4a7a339c89639cbc291ec25fbed6024c7d5b363
Novian Andika <hello@fleetime.my.id>
2022-12-10 07:40:47 +0700

    feat: intro scren done

--- lib/app/modules/auth/views/auth_view.dart
+++ lib/app/modules/auth/views/auth_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 import 'package:get/get.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:zanpakuto_ichigo/app/widget/auth_form.dart';
 
 import '../controllers/auth_controller.dart';
 
@@ -9,15 +13,62 @@ class AuthView extends GetView<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Center(
-            child: Text(
-              'AuthView is working',
-              style: TextStyle(fontSize: 20),
-            ),
+      body: FormBuilder(
+        key: controller.formKey,
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(height: 100),
+              Container(
+                child: Image.asset(
+                  'assets/images/anya.png',
+                  width: 200,
+                ),
+              ),
+              const SizedBox(height: 100),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: AuthForm(
+                  prefixIcon: const Icon(Icons.email),
+                  name: 'email',
+                  ctrl: controller.emailCtrl,
+                  hint: 'Masukkan email anda',
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.email(),
+                  ]),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: AuthForm(
+                  prefixIcon: const Icon(Icons.lock),
+                  name: 'password',
+                  ctrl: controller.passCtrl,
+                  hint: 'Password',
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 50),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: GFButton(
+                    onPressed: () {},
+                    text: 'Login',
+                    fullWidthButton: true,
+                    color: Theme.of(context).primaryColor,
+                    size: GFSize.LARGE,
+                    shape: GFButtonShape.pills,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/localization/l10n.dart';
 
 import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:zanpakuto_ichigo/firebase_options.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,6 +21,19 @@ void main() async {
 
   runApp(
     GetMaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, child!),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(450, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
       title: "AKM Admin",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
